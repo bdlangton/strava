31f7c3ed3a4abdddf878e3701ece86e34dc794ca
Barrett Langton <bdlangton@gmail.com>
2019-02-15 18:44:21 -0700

    Update strava oauth callbacks

--- app/Strava/AuthControllerProvider.php
+++ app/Strava/AuthControllerProvider.php
@@ -27,16 +27,16 @@ class AuthControllerProvider implements ControllerProviderInterface {
     $auth->get('/token_exchange', function (Request $request) use ($app) {
       // Check the session.
       $user = $app['session']->get('user');
-      if (!empty($user)) {
+      $params = $request->query->all();
+      if (!empty($user) || empty($params['code'])) {
         return $app->redirect('/');
       }
 
       // Finish the token exchange.
       $ch = curl_init();
-      $params = $request->query->all();
       curl_setopt($ch, CURLOPT_URL, 'https://www.strava.com/oauth/token');
       curl_setopt($ch, CURLOPT_POST, 3);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, 'client_id=' . $app['client_id'] . '&client_secret=' . $app['client_secret'] . '&code=' . $params['code']);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=authorization_code&client_id=' . $app['client_id'] . '&client_secret=' . $app['client_secret'] . '&code=' . $params['code']);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
       $result = curl_exec($ch);
       $data = json_decode($result, TRUE);
@@ -48,14 +48,21 @@ class AuthControllerProvider implements ControllerProviderInterface {
         )->fetchAll();
         if (!empty($result)) {
           $app['db']->executeQuery(
-            'UPDATE athletes set access_token = ? WHERE id = ?',
-            [$data['access_token'], $data['athlete']['id']]
+            'UPDATE athletes set access_token = ?, refresh_token = ?, token_expires = ? WHERE id = ?',
+            [
+              $data['access_token'],
+              $data['refresh_token'],
+              $data['expires_at'],
+              $data['athlete']['id'],
+            ]
           );
         }
         else {
           $app['db']->insert('athletes', [
             'id' => $data['athlete']['id'],
             'access_token' => $data['access_token'],
+            'refresh_token' => $data['refresh_token'],
+            'token_expires' => $data['expires_at'],
           ]);
         }
         $athlete_data = $app['db']->executeQuery(
@@ -66,6 +73,8 @@ class AuthControllerProvider implements ControllerProviderInterface {
         $app['session']->set('user', [
           'id' => $data['athlete']['id'],
           'access_token' => $data['access_token'],
+          'refresh_token' => $data['refresh_token'],
+          'token_expires' => $data['expires_at'],
           'activity_type' => $athlete_data['default_activity_type'],
           'format' => $athlete_data['default_format'],
         ]);
