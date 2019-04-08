7a813e376f6a8df41ff09ba2755464d494c97332
Barrett Langton <barrett@langton.dev>
2019-04-07 18:08:18 -0600

    Add Command to refresh tokens
    In place of old cron job.

--- src/cron.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$app = require __DIR__ . '/strava.php';
-
-// Check for users that need their tokens refreshed.
-$app['strava']->refreshTokens($app);
