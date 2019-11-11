ec5ec1beacfbbdf94bb730561f320bd88c0824ee
Barrett Langton <barrett@langton.dev>
2019-11-10 18:32:12 -0700

    Break up functions in the webhook controller

--- docroot/src/Controller/WebhookController.php
+++ docroot/src/Controller/WebhookController.php
@@ -36,62 +36,13 @@ class WebhookController extends AbstractController {
     $request = $requestStack->getCurrentRequest();
     $params = $request->getContent();
     $params = (array) json_decode($params);
-    $access_token = $strava->getAccessToken($params['owner_id']);
 
     // Activity type.
     if ($params['object_type'] == 'activity') {
-      // Check if it's a create/update/delete.
-      if ($params['aspect_type'] == 'create') {
-        $activity = $strava->getActivity($params['object_id'], $access_token);
-        $strava->insertActivity($activity);
-      }
-      elseif ($params['aspect_type'] == 'update') {
-        // Set the updates to the appropriate field names and don't include
-        // the 'private' update.
-        $updates = [];
-        foreach ($params['updates'] as $key => $update) {
-          if ($key == 'title') {
-            $updates['name'] = $update;
-          }
-          elseif ($key == 'type') {
-            $updates[$key] = $update;
-          }
-        }
-
-        // Update the existing activity.
-        if ($strava->activityExists($params['object_id'])) {
-          $connection->update('activities',
-            $updates,
-            ['id' => $params['object_id']]
-          );
-        }
-        else {
-          // Even though it's an update, we don't have the activity so we have
-          // to create it.
-          $activity = $strava->getActivity($params['object_id'], $access_token);
-          $strava->insertActivity($activity);
-        }
-      }
-      elseif ($params['aspect_type'] == 'delete') {
-        $connection->delete(
-          'activities',
-          ['id' => $params['object_id']]
-        );
-      }
+      $this->processActivity($params, $strava, $connection);
     }
     elseif ($params['object_type'] == 'athlete') {
-      // For athlete webhooks, we only care if they are deleting.
-      if ($params['aspect_type'] == 'delete') {
-        // Delete all activities and the athlete.
-        $connection->delete(
-          'athletes',
-          ['id' => $params['object_id']]
-        );
-        $connection->delete(
-          'activities',
-          ['athlete_id' => $params['object_id']]
-        );
-      }
+      $this->processAthlete($params, $connection);
     }
 
     // Generate output.
@@ -100,6 +51,82 @@ class WebhookController extends AbstractController {
     return $this->render('webhook.twig', [
       'output' => $output,
     ]);
+  }
+
+  /**
+   * Process an activity webhook.
+   *
+   * @param array $params
+   *   Parameters from teh request.
+   * @param \App\Strava\Strava $strava
+   *   The Strava service.
+   * @param \Doctrine\DBAL\Connection $connection
+   *   The database connection.
+   */
+  private function processActivity(array $params, Strava $strava, Connection $connection) {
+    $access_token = $strava->getAccessToken($params['owner_id']);
+
+    // Check if it's a create/update/delete.
+    if ($params['aspect_type'] == 'create') {
+      $activity = $strava->getActivity($params['object_id'], $access_token);
+      $strava->insertActivity($activity);
+    }
+    elseif ($params['aspect_type'] == 'update') {
+      // Set the updates to the appropriate field names and don't include
+      // the 'private' update.
+      $updates = [];
+      foreach ($params['updates'] as $key => $update) {
+        if ($key == 'title') {
+          $updates['name'] = $update;
+        }
+        elseif ($key == 'type') {
+          $updates[$key] = $update;
+        }
+      }
+
+      // Update the existing activity.
+      if ($strava->activityExists($params['object_id'])) {
+        $connection->update('activities',
+          $updates,
+          ['id' => $params['object_id']]
+        );
+      }
+      else {
+        // Even though it's an update, we don't have the activity so we have
+        // to create it.
+        $activity = $strava->getActivity($params['object_id'], $access_token);
+        $strava->insertActivity($activity);
+      }
+    }
+    elseif ($params['aspect_type'] == 'delete') {
+      $connection->delete(
+        'activities',
+        ['id' => $params['object_id']]
+      );
+    }
+  }
+
+  /**
+   * Process an athlete webhook.
+   *
+   * @param array $params
+   *   Parameters from teh request.
+   * @param \Doctrine\DBAL\Connection $connection
+   *   The database connection.
+   */
+  private function processAthlete(array $params, Connection $connection) {
+    // For athlete webhooks, we only care if they are deleting.
+    if ($params['aspect_type'] == 'delete') {
+      // Delete all activities and the athlete.
+      $connection->delete(
+        'athletes',
+        ['id' => $params['object_id']]
+      );
+      $connection->delete(
+        'activities',
+        ['athlete_id' => $params['object_id']]
+      );
+    }
   }
 
 }
