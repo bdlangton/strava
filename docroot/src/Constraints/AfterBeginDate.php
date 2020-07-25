fe4ae3d16271f7c87595b7ef94770da726d5a81d
Barrett Langton <barrett@langton.dev>
2020-07-25 15:47:08 -0600

    Update date constraints to use a custom compareValues

--- docroot/src/Constraints/AfterBeginDate.php
+++ docroot/src/Constraints/AfterBeginDate.php
@@ -8,7 +8,13 @@ use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
  * @Annotation
  * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
  */
-class AfterBeginDate extends GreaterThanOrEqual
-{
-    public $message = 'This value should be after the begin date.';
+class AfterBeginDate extends GreaterThanOrEqual {
+
+  /**
+   * The message to display when there is an error.
+   *
+   * @var string
+   */
+  public $message = 'This value should be after the begin date.';
+
 }
--- docroot/src/Constraints/AfterBeginDateValidator.php
+++ docroot/src/Constraints/AfterBeginDateValidator.php
@@ -7,7 +7,20 @@ use Symfony\Component\Validator\Constraints\GreaterThanOrEqualValidator;
 /**
  * Validates values are greater than or equal to the previous (>=).
  */
-class AfterBeginDateValidator extends GreaterThanOrEqualValidator
-{
+class AfterBeginDateValidator extends GreaterThanOrEqualValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function compareValues($value1, $value2) {
+    if (empty($value1) || empty($value2)) {
+      return TRUE;
+    }
+
+    $value1 = new \DateTime($value1);
+    $value2 = new \DateTime($value2);
+
+    return $value1->getTimestamp() >= $value2->getTimestamp();
+  }
 
 }
