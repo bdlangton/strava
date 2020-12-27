f058ca1c48c8aa06df6e8e8d387a6a63bd022c42
Barrett Langton <barrett@langton.dev>
2020-12-27 13:01:50 -0700

    Abandon white-october/pagerfanta-bundle package

--- docroot/config/bundles.php
+++ docroot/config/bundles.php
@@ -5,6 +5,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
